d553f43cc172a7ce025c7d396bdeef1f4508662e
dana <danaw09@gmail.com>
2019-07-17 07:44:35 -0500

    added another member variable to contestant class

---
+++ SweepStakes/SweepStakes/SweepStakes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class SweepStakes
+    {
+        public string firstName;
+        public string Contestant;
+    }
+   
+}

517dd00bacccd60b757257e0552d6bb4c21f09fb
dana <danaw09@gmail.com>
2019-07-17 09:06:04 -0500

    added que method to sweepstakesqueue manager

--- SweepStakes/SweepStakes/SweepstakesQueueManager.cs
+++ SweepStakes/SweepStakes/SweepstakesQueueManager.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SweepStakes
 {
-    class SweepstakesQueueManager: SweepstakesManager_interface
+    class SweepstakesQueueManager : SweepstakesManager_interface
     {
+        public void EnqueueOnQueue()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine("Elements in queue: ");
+            foreach (int number in queue)
+            {
+                Console.WriteLine(number);
+            }
+        }
     }
 }

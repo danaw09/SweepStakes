094fda2e9fb9577be7ebf0adf8e52fef0ad7f114
dana <danaw09@gmail.com>
2019-07-16 21:03:44 -0500

    added contestant variables and also added ctor for contestant with appropriate string for firstname lastname email

---
+++ SweepStakes/SweepStakes/Contestant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string dob;
+        public string postalCode;
+
+       
+        public Contestant(string _firstName, string _lastName, string _email, string _dob, string _postalCode = null)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            email = _email;
+            dob = _dob;
+            postalCode = _postalCode;
+        }
+    }
+}
